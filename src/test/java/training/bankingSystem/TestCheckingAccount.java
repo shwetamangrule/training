381d14b932e480ee4562971769baf321a1b8f532
trainee <prateekkr1295@gmail.com>
2018-04-24 16:28:49 +0530

    testcase on checking account added

--- src/test/java/training/bankingSystem/TestCheckingAccount.java
+++ src/test/java/training/bankingSystem/TestCheckingAccount.java
@@ -7,22 +7,32 @@ import org.junit.Test;
 public class TestCheckingAccount {
 
 	Money money=new Money();
+	
 	CheckingAccount chknAcnt=new CheckingAccount();
+	
 	
 	@Test
 	public void testdeposit(){
 		
-				
-				//System.out.println(money.getMoney());
-				assertEquals("testmessage",600,chknAcnt.deposit(100));
+				money.setMoney(0);
+				int m = money.getMoney() + 100;
+				System.out.println(m);
+				money.setMoney(m);
+				System.out.println(money.getMoney());
+				assertEquals("testmessage",money.getMoney(),chknAcnt.deposit(100));
 		
 	}
 	@Test
 	public void testwithdrawl(){
 		
-				
-				//System.out.println(money.getMoney());
-				assertEquals("testmessage",400,chknAcnt.withdrawl(200));
+		
+				int m = money.getMoney() - 200;
+				System.out.println(m);
+
+				money.setMoney(m);
+				System.out.println(money.getMoney());
+
+				assertEquals("testmessage",money.getMoney(),chknAcnt.withdrawl(200));
 		
 	}
 	
