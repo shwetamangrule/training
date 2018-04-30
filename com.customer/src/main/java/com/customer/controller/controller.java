38e0e7ec24d03fc2ec83d753f9faed017e9a0999
trainee <prateekkr1295@gmail.com>
2018-04-30 11:33:50 +0530

    waoo

--- com.customer/src/main/java/com/customer/controller/controller.java
+++ com.customer/src/main/java/com/customer/controller/controller.java
@@ -12,25 +12,32 @@ public class controller {
 	public static void main(String[] args) {
 
 		ApplicationContext appContext=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		/*
+		 * adding a new customer
+		 */
 		Address address=(Address) appContext.getBean("address");
 		Address address1=(Address) appContext.getBean("addressRetail");
 		CustomerService custService=(CustomerService)appContext.getBean("customerService");
 		Customer customer=(Customer) appContext.getBean("customer");
 		custService.addCustomer(customer);
-		System.out.println(custService.viewCustomer());
-
+		System.out.println(custService.viewCustomer());	//view all customer
+/*
+ * adding another customer
+ */
 		Customer customer1=(Customer) appContext.getBean("customer1");
 		custService.addCustomer(customer1);
-		System.out.println(custService.viewCustomer());
+		System.out.println(custService.viewCustomer());			//view all customer
 
+		/*
+		 * removing a customer
+		 */
 		custService.removeCustomer(customer1);
-		System.out.println(custService.viewCustomer());
+		System.out.println(custService.viewCustomer());	//view all customer
 
 		Customer customerU=(Customer) appContext.getBean("customerU");
 		custService.updateCustomer(customerU);
 		System.out.println(custService.viewCustomer());
 
-		
 	}
 
 }
