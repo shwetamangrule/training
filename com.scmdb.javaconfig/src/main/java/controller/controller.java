0f1746798484fb73b8d18a9ff3c0eafcc3c4ca75
trainee <prateekkr1295@gmail.com>
2018-05-03 13:59:36 +0530

    calling junit from controller

--- com.scmdb.javaconfig/src/main/java/controller/controller.java
+++ com.scmdb.javaconfig/src/main/java/controller/controller.java
@@ -1,10 +1,18 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import Service.CustomerService;
+import Service.CustomerServiceImpl;
 import Service.GoodsService;
+import Service.GoodsServiceImpl;
+import Service.RetailerService;
+import Service.RetailerServiceImpl;
 import Service.SupplierService;
 import Service.SupplierServiceImpl;
 import model.Customer;
@@ -13,21 +21,74 @@ import model.Supplier;
 
 public class controller {
 
-	public static void main(String[] args) {
+	ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigApp.class);
 
-		 ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
-	      	
-			CustomerService custService = (CustomerService) context.getBean("CustomerService");
-			Customer customer = new Customer(106, "TuhinDas", "Wb", "credit", 101);
-			System.out.println(custService.addCustomer(customer));
-			
-			Goods goods=new Goods(1003,"MangoBite",12,30,104);
-			GoodsService goodsService=(GoodsService) context.getBean("GoodsService");
-			System.out.println(goodsService.addGoods(goods));
-		 
-		 SupplierService supplierService=(SupplierServiceImpl)context.getBean("SupplierService");
-		 Supplier supplier=new Supplier(105,"lol","chennai",20,112,20,103);
-		 System.out.println(supplierService.addSupplier(supplier));
+	final CustomerService customerService = ctx.getBean("CustomerService", CustomerServiceImpl.class);
+	final SupplierService supplierService = ctx.getBean("SupplierService", SupplierServiceImpl.class);
+	final GoodsService goodsService = ctx.getBean("GoodsService", GoodsServiceImpl.class);
+	final RetailerService retailerService = ctx.getBean("RetailerService", RetailerServiceImpl.class);
+
+	public int addCustomerControl() {
+		Customer customer = new Customer(251, "Tuhin", "Wb", "credit", 101);
+		Customer customer1 = new Customer(252, "Sukumar", "Wb", "credit", 101);
+		customerService.addCustomer(customer1);
+		return customerService.addCustomer(customer);
+		}
+
+	public int deleteCustomerControl() {
+	 return customerService.removeCustomer(252);
+	}
+
+	public int updateCustomerControl() {
+		Customer customer = new Customer(253, "Arati", "moheshpur", "debit", 101);
+		customerService.addCustomer(customer);
+		return customerService.updateCustomer("cash", 253);
+	}
+
+	public int addSupplierControl() {
+		Supplier supplier = new Supplier(351, "patanjali", "mumbai", 20, 112, 20, 101);
+		Supplier supplier1 = new Supplier(352, "gobordhan", "mumbai", 30, 112, 40, 101);
+		supplierService.addSupplier(supplier1);
+		return supplierService.addSupplier(supplier);
+	
+	}
+
+	public int updateSupplierControl() {
+		return supplierService.updateSupplier(352, 50);
+	}
+
+	public int deleteSupplierControl() {
+		return supplierService.removeSupplier(351);
+	}
+
+	public int addGoodsControl() {
+		Goods goods = new Goods(451, "kitkat", 12, 30, 301);
+		Goods goods1 = new Goods(452, "kurkure", 12, 40, 301);
+		goodsService.addGoods(goods1);
+		return goodsService.addGoods(goods);
+	}
+
+	public int removeGoodsControl() {
+		return goodsService.removeGoods(451);
+
+	}
+
+	public int updateGoodsControl() {
+		return goodsService.updategoods(452, 50);
+	}
+
+	public int viewgoodsControl() {
+		List<Goods> goodslist = retailerService.viewGoods();
+		return 1;
+	}
+
+	public int viewCustomer() {
+		List<Customer> customerlist = retailerService.viewCustomer();
+		return 1;
+	}
+	public int viewSupplier() {
+		List<Supplier> viewsupplier = retailerService.viewSupplier();
+		return 1;
 	}
 
 }
--- com.scmdb.javaconfig/src/test/java/com/javaconfic/retailTest/Testcases.java
+++ com.scmdb.javaconfig/src/test/java/com/javaconfic/retailTest/Testcases.java
@@ -1,126 +1,85 @@
 package com.javaconfic.retailTest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.List;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import Service.CustomerService;
-import Service.CustomerServiceImpl;
-import Service.GoodsService;
-import Service.GoodsServiceImpl;
-import Service.RetailerService;
-import Service.RetailerServiceImpl;
-import Service.SupplierService;
-import Service.SupplierServiceImpl;
-import controller.ConfigApp;
+import controller.controller;
 import model.Customer;
-import model.Goods;
-import model.Supplier;
 
 public class Testcases {
 
-	 ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigApp.class);
-	 
-	final CustomerService customerService = ctx.getBean("CustomerService", CustomerServiceImpl.class);
-    final SupplierService supplierService=ctx.getBean("SupplierService",SupplierServiceImpl.class);
-    final GoodsService goodsService=ctx.getBean("GoodsService",GoodsServiceImpl.class);
-    final RetailerService retailerService=ctx.getBean("RetailerService",RetailerServiceImpl.class);
-
+	controller cntrl=new controller();
+	
 	@org.junit.Test
 	public void addCustomerTest() {
-		Customer customer = new Customer(221, "Tuhin", "Wb", "credit", 101);
-		Customer customer1 = new Customer(222, "Sukumar", "Wb", "credit", 101);
-		customerService.addCustomer(customer1);
-		int value=customerService.addCustomer(customer);
-		assertEquals(221, value);
+				assertEquals(221, cntrl.addCustomerControl());
 	}
 	@Test 
 	public void deleteCustomerTest()
 	{
-		long value=customerService.removeCustomer(222);
-		assertEquals(1, value);
+		assertEquals(1, cntrl.deleteCustomerControl());
 }
 
   @Test
 public void updateCustomer()
 {
-   
-	Customer customer=new Customer(223,"Arati","moheshpur","debit",101);
-	customerService.addCustomer(customer);
-	long value=customerService.updateCustomer("cash", 223);
-	assertEquals(1, value);
+   	assertEquals(1, cntrl.updateCustomerControl());
 }
-
 @Test
 public void addSupplier()
 {
-	Supplier supplier=new Supplier(311,"patanjali","mumbai",20,112,20,101);
-	Supplier supplier1=new Supplier(312,"gobordhan","mumbai",30,112,40,101);
-	supplierService.addSupplier(supplier1);
-   long value=supplierService.addSupplier(supplier);
-   assertEquals(311,value);
+	 
+   assertEquals(311,cntrl.addSupplierControl());
  }
 
   @Test
 public void updateSupplier()
 {
-  long value= supplierService.updateSupplier(312, 50);
-   assertEquals(1,value);
+ ;
+   assertEquals(1,cntrl.updateSupplierControl());
 }
 
 @Test
 public void deleteSupplier() {
-   
-   assertEquals(1,supplierService.removeSupplier(312));
+      assertEquals(1,cntrl.deleteSupplierControl());
       }
 
  @Test
 public void addGoods()
 {
-	 Goods goods=new Goods(411,"kitkat",12,30,301);
-	 Goods goods1=new Goods(412,"kurkure",12,40,301);
-	 goodsService.addGoods(goods1);
-   long value=goodsService.addGoods(goods);
-   assertEquals(411,value);
+	
+   assertEquals(411,cntrl.addGoodsControl());
 }
 
 @Test
 public void removeGoods()
 {
-   long value=goodsService.removeGoods(411);
-   assertEquals(1,value);
+     assertEquals(1,cntrl.removeGoodsControl());
 }
 
   
 @Test
 public void updateGoods() {
-   long value=goodsService.updategoods(412, 50);
-   assertEquals(1,value);
+  
+   assertEquals(1,cntrl.updateGoodsControl());
    
 }
 
     @Test
     public void viewgoods()
     {
-   	List<Goods> goodslist= retailerService.viewGoods();
-    assertTrue(goodslist.contains(goodslist.get(0)) );
+   	    assertEquals(1, cntrl.viewgoodsControl());
     }
     
     @Test
     public void viewCustomer()
     {
-   	List<Customer> customerlist= retailerService.viewCustomer();
-    assertTrue(customerlist.contains(customerlist.get(1)) );
+    	assertEquals(1, cntrl.viewCustomer());
     }
     @Test
     public void viewSupplier()
     {
-   	List<Supplier> viewsupplier= retailerService.viewSupplier();
-    assertTrue(viewsupplier.contains(viewsupplier.get(1)) );
+    	assertEquals(1, cntrl.viewSupplier());
     }
 }
