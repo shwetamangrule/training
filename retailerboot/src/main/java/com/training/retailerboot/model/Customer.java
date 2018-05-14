e8b5754dd54abc180c9a86dfaf2c089a451b4797
trainee <prateekkr1295@gmail.com>
2018-05-14 12:57:54 +0530

    calling endpoints using java main methods

--- retailerboot/src/main/java/com/training/retailerboot/model/Customer.java
+++ retailerboot/src/main/java/com/training/retailerboot/model/Customer.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.training.retailerboot.annotation.AnnotationMy;
 
